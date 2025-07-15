440263dee6ac66a17d66ff0b7ecc0f9e5e4fd7dd
valera <valeri.kuzmini123@gmail.com>
2025-07-15 17:45:50 +0400

    added and connected scss

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { geistMono, geistSans } from "@/shared/fonts";
-import "@/app/globals.css";
+import "@/app/scss/styles.scss";
 
 export const metadata: Metadata = {
   title: "Create Next App",
