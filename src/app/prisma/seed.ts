1b5cad8d7abd9c037f4cf15aba0ed1df0dd3769d
valera <valeri.kuzmini123@gmail.com>
2025-08-08 14:26:20 +0400

    fix: fixed error, when seeding existed user in prisma

--- src/app/prisma/seed.ts
+++ src/app/prisma/seed.ts
@@ -1,6 +1,20 @@
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
-async function main() {}
+import { prisma } from "./prisma-client";
+
+async function up() {}
+
+async function down() {
+  await prisma.$executeRaw`TRUNCATE TABLE "user" RESTART IDENTITY CASCADE`;
+}
+
+async function main() {
+  try {
+    await down();
+    await up();
+  } catch (e) {
+    console.error("Error seeding the database", e);
+  }
+}
+
 main()
   .then(async () => {
     await prisma.$disconnect();
